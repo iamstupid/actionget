523b9d9b82a17969b75395ee49ef956d494517c4
Tommy Zng <1319524930@qq.com>
2013-12-15 15:19:32 +0800

    Add Callback API

--- get.js
+++ get.js
@@ -1,4 +1,23 @@
-(function(){
+window.getRegisteredCallbacks=function(m){
+            return (var x=window['~reg~callbacks~'+m]);
+}
+window.registerCallback=function(m,f){
+            if(window['~status~done~'+m]){
+                        f();
+            }else{
+                        if(window['~reg~callbacks~'+m]===undefined){
+                                    window['~reg~callbacks~'+m]=[f];
+                                    window['~status~done~'+m]=false;
+                        }else{
+                                    window['~reg~callbacks~'+m].push(f);
+                        }
+            }
+}
+window.clearRegisteredCallbacks=function(m){
+            delete window['~reg~callbacks~'+m];
+            delete window['~status~done~'+m];
+}
+(function(callback){
             function toChar(nch) {
                 var f = parseInt(nch, 16);
                 return String.fromCharCode(f);
@@ -9,4 +28,9 @@
                 var f = k[i].split("=");
                 $_GET[f[0]] = f[1].replace(/%[0-9A-Fa-f][0-9A-Fa-f]/g, toChar);
             }
-})();
+            window["~status~done~module$_GET"]=true;
+            window.clearRegisteredCallbacks("module$_GET");
+            for(var i in callback){
+                        callback[i]($_GET);
+            }
+})(window.getRegisteredCallbacks("module$_GET"));
