d6baf948cdc59ba76bcd185785f74f3a526198a9
Tommy Zng <1319524930@qq.com>
2013-12-15 14:22:33 +0800

    Create get.js

---
+++ get.js
@@ -0,0 +1,12 @@
+(function(){
+            function toChar(nch) {
+                var f = parseInt(nch, 16);
+                return String.fromCharCode(f);
+            }
+            window["$_GET"] = {};
+            var k = location.href.split("?")[1].split("&");
+            for (var i in k) {
+                var f = k[i].split("=");
+                $_GET[f[0]] = f[1].replace(/%[0-9A-Fa-f][0-9A-Fa-f]/g, toChar);
+            }
+})();
